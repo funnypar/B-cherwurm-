ef441efd944deeaa68e192bf2caba4f3c612c800
Mopano <p.r.7.norouzi@gmail.com>
2022-11-20 01:09:50 -0800

    Added box

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        hbox: "90vh",
+      },
+      width: {
+        wbox: "92vw",
+      },
+    },
   },
   plugins: [],
 };
